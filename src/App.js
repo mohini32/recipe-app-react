cbfef9353bdf335eee37d51f8b5f91e8bc8a1b78
mohinieassy <mohini.m@eassyserve.in>
2023-06-14 11:17:10 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import Header from './Components/Header'
+import Tabs from './Components/Tabs'
+import RecipeLists from './Components/RecipeLists';
+function App() {
+  return (
+    <div id=''>
+  <Header/>
+  <RecipeLists/>
+  <Tabs/></div>
+  );
+}
+
+export default App;
